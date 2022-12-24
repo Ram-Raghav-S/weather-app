5b2c16c46bc6fdcaea4947e73db0ba321628dcd8
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2022-12-24 17:34:34 -0500

    app wide styles and location search box added

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const api = {
   key: "839fe94c6921794a7c2aa11baaf2fb58",
-  base: "https://api.openweathermap.ord/data/2.5/"
+  base: "https://api.openweathermap.ord/data/2.5/",
 };
-
-
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search for your location"
+          />
+        </div>
+        
+      </main>
     </div>
   );
 }
