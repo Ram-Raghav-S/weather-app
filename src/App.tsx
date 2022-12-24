d7f0b6d4f1294b94bdc5bfc72e37ae3a7542a229
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2022-12-24 14:19:45 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const api = {
+  key: "839fe94c6921794a7c2aa11baaf2fb58",
+  base: "https://api.openweathermap.ord/data/2.5/"
+};
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
