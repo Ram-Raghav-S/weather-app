68646c5794815e616af5acfab3269cbbe7b8774e
Ram-Raghav-S <rraagghhaavv21@gmail.com>
2022-12-30 14:55:55 -0500

    Added weather looking functionality through api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const api = {
-  key: "839fe94c6921794a7c2aa11baaf2fb58",
-  base: "https://api.openweathermap.ord/data/2.5/",
+// TODO: implement to fix api leak
+const getWeatherApiKey = async () => {
+  let res: any = await fetch("./private.json");
+};
+
+const weatherApi = {
+  key: "839fe94c6921794a7c2aa11baaf2fb58", // TODO: fix key leak
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
+  const [query, setQuery] = useState<string>("");
+  const [weather, setWeather] = useState<any>();
+
+  const search = (event: { key: string }) => {
+    if (event.key === "Enter") {
+      fetch(
+        `${weatherApi.base}weather?q=${query}&units=metric&APPID=${weatherApi.key}`
+      )
+        .then((result) => result.json())
+        .then((result) => {
+          console.log(result);
+          setWeather(result);
+        });
+    }
+  };
+
+
+  const titleCase = (sentence: string) => {
+    const words = sentence.toLowerCase().split(" ");
+    for(var i = 0; i < sentence.length; i++) {
+      words[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+    return words.join(" ")
+  }
+
   return (
     <div className="app">
       <main>
@@ -15,9 +45,28 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search for your location"
+            onChange={(event) => setQuery(event.target.value)}
+            value={query}
+            onKeyDown={search}
           />
         </div>
-        
+        <div>
+          <div className="location-box">
+            <div className="location">{titleCase(query)}</div>
+            <div className="date">
+              {new Date().toLocaleDateString("en-GB", {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="weather-box">
+          <div className="temperature">{Math.round(weather?.main.temp)}°C</div>
+          <div className="weather">{weather?.weather[0].main}</div>
+        </div>
       </main>
     </div>
   );
